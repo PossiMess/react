bb16adc91ec898575c383a9ed19d6fa489ccc264
Heonyoung Lee <ddag6714@gmail.com>
2021-10-28 20:44:52 +0900

    map function, propTypes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import React from "react";
+import propTypes from "prop-types";
 
-function Fruits({name}) {
+function Fruits({name, flavour}) {
   return (
-    <div>I Like {name}.</div>
+    <div>
+      <p>I eat {name}.</p>
+      <p>the flavour is {flavour}.</p>
+    </div>
   )
 }
 
+Fruits.propTypes = {
+  name: propTypes.string.isRequired,
+  flavour: propTypes.string.isRequired
+};
+
+const FruitsIlike =[
+  {
+    id:1,
+    name: "apple",
+    flavour: "sweet"
+  },
+  {
+    id:2,
+    name: "banana",
+    flavour: "sweet"
+  },
+  {
+    id:3,
+    name: "pear",
+    flavour: "seek"
+  },
+  {
+    id:4,
+    name: "melon",
+    flavour: "sweet"
+  },
+]
+function fruitsRender(dish){
+  return <Fruits key={dish.id}name={dish.name} flavour={dish.flavour} />
+}
 function App() {
   return (
-    <div>Hello!!
-      <Fruits name="apple"/>
-      <Fruits name="banana"/>
-      <Fruits name="pear"/>
-      <Fruits name="melon"/>
-
+    <div>
+      {
+      FruitsIlike.map(fruitsRender)
+      }
     </div>
 
   );
